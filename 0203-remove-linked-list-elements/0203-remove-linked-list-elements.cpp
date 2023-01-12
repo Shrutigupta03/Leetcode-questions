c50cd9648dd132d51db4ad5a9ba7ae384c639d73
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2023-01-12 21:06:50 +0530

    Time: 20 ms (94.86%), Space: 15 MB (51.29%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -10,14 +10,28 @@
  */
 class Solution {
 public:
-    ListNode* removeElements(ListNode* head, int value) {
-         while(head != NULL && head->val == value){
+    ListNode* removeElements(ListNode* head, int val) {
+          while(head != NULL && head->val == val) {
             head = head->next;
         }
-        if(head == NULL)
-            return head;
+        if(head == NULL){
+            return NULL;
+        }
         
-        head->next = removeElements(head->next,value);
+        ListNode* pointer;
+        pointer = head;
+
+        while (pointer->next != NULL)  // removing ftom middle of the linked list
+        {
+            if(pointer->next->val == val)
+            {
+                pointer->next = pointer->next->next;
+            }
+            else
+            {
+                pointer = pointer->next;    
+            }
+        }
         return head;
     }
 };

e40a60bd11056c17e74bc1165b337de8fe6432b4
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2023-01-21 22:30:32 +0530

    Time: 5 ms (42.51%), Space: 6.5 MB (70.83%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     bool check(string s){
         int n=s.size();
-
         int x=stoi(s);
         if((n>1 && s[0]=='0') || x>255){
             return false;
         }
-        
         return true;
     }
+    
     vector<string> restoreIpAddresses(string s) {
         vector<string> vs;
-         if(s.size()>12){
+        int n=s.size();
+         if(n>12){
              return vs;
          }
-        int n=s.size();
+        
         for(int i=1; i<=3; i++){
             for(int j=1; j<=3; j++){
                 for(int k=1; k<=3; k++){

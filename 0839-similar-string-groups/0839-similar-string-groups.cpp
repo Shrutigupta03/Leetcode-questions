fba4e7004e07085e7267321a6b9d1909fdb71002
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2023-04-29 00:45:46 +0530

    Time: 355 ms (21.27%), Space: 106.9 MB (30.48%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.cpp
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -1,43 +1,31 @@
 class Solution {
-private:
-    bool isSimilar(string s1, string s2) {
-        int count = 0;
-        for (int i = 0; i < s1.length(); i++) {
-            if (s1[i] != s2[i]) {
-                count++;
-                if (count > 2) {
-                    return false;
-                }
-            }
-        }
-        return count == 2 || count == 0;
-    }
-
-    void dfs(string str, vector<string>& strs, set<string>& visited) {
-        if (visited.count(str) > 0) {
-            return;
-        }
-        visited.insert(str);
-        for (int i = 0; i < strs.size(); i++) {
-            if (isSimilar(str, strs[i])) {
-                dfs(strs[i], strs, visited);
-            }
-        }
-    }
-
 public:
     int numSimilarGroups(vector<string>& strs) {
-        int result = 0;
-        if (strs.empty()) {
-            return 0;
+        int groups = 0, n = strs.size();
+        vector<bool> vis(n, false);
+        for(int i=0;i<strs.size();i++){
+            if(vis[i]) continue;
+            groups++;
+            dfs(i, strs, vis);
         }
-        set<string> visited;
-        for (string it : strs) {
-            if (visited.count(it) == 0) {
-                dfs(it, strs, visited);
-                result++;
+        return groups;
+    }
+
+    void dfs(int i, vector<string>& strs, vector<bool>& vis){
+        vis[i] = true;
+        for(int j=0;j<strs.size();j++){
+            if(vis[j]) continue;
+            if(is_similar(strs[i], strs[j])){
+                dfs(j, strs, vis);
             }
         }
-        return result;
+    }
+
+    bool is_similar(string a, string b){
+        int count=0;
+        for(int i=0; i<a.length(); i++){
+            if(a[i] != b[i]) count++;
+        }
+        return (count == 2 || count == 0);
     }
 };

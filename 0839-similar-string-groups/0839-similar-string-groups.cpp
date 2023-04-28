cd7571e2dbf17d2db17cf1c03209b157c9856731
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2023-04-29 00:47:37 +0530

    Time: 32 ms (77.78%), Space: 13.9 MB (39.05%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.cpp
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -1,31 +1,45 @@
 class Solution {
 public:
-    int numSimilarGroups(vector<string>& strs) {
-        int groups = 0, n = strs.size();
-        vector<bool> vis(n, false);
-        for(int i=0;i<strs.size();i++){
-            if(vis[i]) continue;
-            groups++;
-            dfs(i, strs, vis);
-        }
-        return groups;
-    }
-
-    void dfs(int i, vector<string>& strs, vector<bool>& vis){
-        vis[i] = true;
-        for(int j=0;j<strs.size();j++){
-            if(vis[j]) continue;
-            if(is_similar(strs[i], strs[j])){
-                dfs(j, strs, vis);
+    bool check(const string &a,const string &b){
+        int c = 0;
+        for(int i = 0; i < a.size(); i++){
+            if(a[i] != b[i]){
+                c++;
+                if(c>2)return false;
             }
         }
+        return c == 0 || c == 2;
     }
-
-    bool is_similar(string a, string b){
-        int count=0;
-        for(int i=0; i<a.length(); i++){
-            if(a[i] != b[i]) count++;
+    int numSimilarGroups(vector<string>& strs) {
+        vector<vector<int>> adj(strs.size());
+        for(int i = 0; i < strs.size(); i++){
+            for(int j = i+1; j < strs.size(); j++){
+                if(check(strs[i],strs[j])){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
         }
-        return (count == 2 || count == 0);
+        int res = 0;
+        vector<bool> visited(strs.size()+1);
+        for(int i = 0; i < strs.size(); i++){
+            if(!visited[i]){
+                res++;
+                queue<int> q;
+                q.push(i);
+                while(!q.empty()){
+                    int cur = q.front();
+                    q.pop();
+                    visited[cur] = true;
+                    for(int j = 0; j < adj[cur].size(); j++){
+                        if(!visited[adj[cur][j]]){
+                            visited[adj[cur][j]] = true;
+                            q.push(adj[cur][j]);
+                        }
+                    }
+                }
+            }
+        }
+        return res;
     }
 };

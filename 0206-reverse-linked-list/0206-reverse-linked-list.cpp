4a53a9dc5a7a0abfa2a80d8cbbf2c4f7fc4ff45d
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2023-07-26 00:56:36 +0530

    Time: 7 ms (59.78%), Space: 8.3 MB (56.46%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -12,27 +12,17 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         struct ListNode *p, *q, *r;
-
-        // if an empty list is given , return the same list
         if(!head || !head->next) return head;
-
-        // initial positions of pointers
         p = head;
         q = p->next;
         r = q->next;
-
-        // current head becomes last element
-        // head->next needs to point to NULL
         head->next = NULL;
-        
-        // reversing links by traversing the list
         while(r){
             q->next = p;
             p = q;
             q = r;
             r = r->next;    
         }
-
         q->next = p;
         return q;
     }

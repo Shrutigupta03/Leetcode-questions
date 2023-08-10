4fc832ffa310933293d4206d472d01bfc21cd024
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2023-08-11 02:11:17 +0530

    Time: 9 ms (51.05%), Space: 6.9 MB (36.49%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -8,32 +8,18 @@ public:
         }
         s = countAndSay(n-1);
         string ans;
-        if(s.size()==1){
-            ans.push_back('1');
-            ans.push_back(s[0]);
-        }
-        else{
-            for(int i=0; i<s.size(); i++){
-                int x=1;
-                if(i<=s.size()-2 && s[i]==s[i+1]){
-                    while(s[i]==s[i+1]){
-                        i++;
-                        x++;
-                    }
-                }
-                
-                ans += to_string(x);
-                if(x==1){
-                    // cout<<"last if x "<<x<<endl;
-                    ans.push_back(s[i]);
-                }
-                else{
-                    // cout<<"last else x "<<x<<endl;
-                    ans.push_back(s[i-1]);
+        for(int i=0; i<s.size(); i++){
+            int x=1;
+            if(i<s.size()-1 && s[i]==s[i+1]){
+                while(s[i]==s[i+1]){
+                    i++;
+                    x++;
                 }
             }
+            ans += to_string(x);
+            ans.push_back(s[i]);
         }
-     return ans;
+        return ans;
     }
    
 };

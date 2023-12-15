f7eca2abd20ab50a2ede8b34deff547eb37f8d84
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2023-12-15 09:11:43 +0530

    Time: 14 ms (19.38%), Space: 11.2 MB (39.00%) - LeetHub

--- 1436-destination-city/1436-destination-city.cpp
+++ 1436-destination-city/1436-destination-city.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
-        string ans;
-        int n = paths.size();
-        vector<int> vis(n, 0);
-        queue<string> q;
-        q.push(paths[0][1]);
-        vis[0]=1;
-        while(!q.empty()){
-            auto temp = q.front();
-            q.pop();
-            for(int i=1; i<n; i++){
-                if(vis[i]==0 && paths[i][0]==temp){
-                    q.push(paths[i][1]);
-                    vis[i]=1;
-                }
-            }
-            ans = temp;
+        int m = paths.size();
+        
+        unordered_map<string, int> mp;
+        for(int i = 0; i < m; i++)
+           mp[paths[i][0]]++;
+        
+        for(int i = 0; i < m; i++)
+        {
+            if(mp[paths[i][1]] < 1)
+                return paths[i][1];
         }
-        return ans;
+        
+        return "";  
     }
 };

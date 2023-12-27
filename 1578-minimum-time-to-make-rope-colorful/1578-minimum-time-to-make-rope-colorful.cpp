55a3d117d64bc9a39197817edbe2cee6ea9158c7
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2023-12-27 15:39:50 +0530

    Time: 133 ms (20.42%), Space: 95.6 MB (83.31%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,37 +1,17 @@
 class Solution {
 public:
-    int minCost(string colors, vector<int>& neededTime) {
-        char curr=colors[0];
-        int res=0, len = colors.size();
-        multiset<int> s;
-        s.insert(neededTime[0]);
-        for(int i=1; i<len; i++){
-            if(colors[i]==curr){
-                s.insert(neededTime[i]);
-            }
-            else{
-                if(s.size()>1){
-                    int sz = s.size(), x=0;
-                    for(auto it : s){
-                        if(x==sz-1) continue;
-                        res+=it;
-                        x++;
-                    }
-                }
-                s.clear();
-                curr=colors[i];
-                s.insert(neededTime[i]);
-            }
+    int minCost(string s, vector<int>& neededTime) {
+        int res = 0;
+        int n = s.size();
+        for(int i = 1; i < n; i++)
+        {
+            if(s[i] == s[i - 1])
+            {
+                res += min(neededTime[i], neededTime[i - 1]); 
+                neededTime[i] = max(neededTime[i], neededTime[i - 1]); 
+            }     
         }
         
-        if(s.size()>1){
-            int sz = s.size(), x=0;
-            for(auto it : s){
-                if(x==sz-1) continue;
-                res+=it;
-                x++;
-            }
-        }
         return res;
     }
 };

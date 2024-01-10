fd0b8d6751185a4f6edbe9047d1521c242ceafbc
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2024-01-11 00:46:49 +0530

    Time: 42 ms (22.16%), Space: 7.1 MB (48.18%) - LeetHub

---
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int MOD = 1e9+7;
+    
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        vector<long> memo(n+1,1); 
+        memo[0]=0;  
+        
+        for(int i=1;i<=n;i++)
+            for(int j=delay;j<forget;j++) 
+                if(i-j>=0)
+                    memo[i]=(memo[i]+memo[i-j])%MOD;   
+        
+        return (memo[n]-memo[n-forget]+MOD)%MOD;  
+    }
+};

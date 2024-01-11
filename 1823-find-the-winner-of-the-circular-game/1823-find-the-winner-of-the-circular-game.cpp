3c6e54e81b46826443f8f508c868bf34e43de5ec
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2024-01-11 14:42:06 +0530

    Time: 79 ms (5.07%), Space: 6.6 MB (43.67%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        // vector<int> v;
         vector<int> vis(n+1, 0);
-        
-        // for(int i=0; i<n; i++){
-        //     v.push_back(i);
-        // }
+       
         int x=0;
-        int cnt=0;
+        int cnt=0;            
+        int res;
+        
         for(int i=1; i<=n; i++){
-            if(x==n-1){
+            if(x==n){
                  break;
             }
             
@@ -18,24 +16,16 @@ public:
                 if(i==n) i=0;
                 continue;
             }
-            // cout<<i<<" ";
             cnt++;
             if(cnt==k){
                 cnt=0;
                 vis[i]=1;
-                // cout<<i<<" ";
+                res = i;
                 x++; 
-                // cout<<"   "<<x<<endl;
             }   
             if(i==n) i=0;
         }
-        
-        int res;
-        for(int i=1; i<=n; i++){
-            if(vis[i]==0){
-                res = i;
-            }
-        }
+       
         return res;
     }
 };

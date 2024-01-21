8bfc9026f60e7639e6f29b1af7d0681cd54fd2ac
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2024-01-21 14:28:06 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -5,11 +5,14 @@ public:
         if (n == 0) return 0;
         if (n == 1) return nums[0];
         if (n == 2) return max(nums[0], nums[1]);
-        vector<int> f(n, 0);
-        f[0] = nums[0];
-        f[1] = max(nums[0], nums[1]);
-        for (int i = 2; i < n; ++i)
-            f[i] = max(f[i-2] + nums[i], f[i-1]);
-        return f[n-1];
+        // vector<int> f(n, 0);
+        int prev2 = nums[0];
+        int prev1 = max(nums[0], nums[1]);
+        for (int i = 2; i < n; ++i){
+            int curr = max(prev2 + nums[i], prev1);
+            prev2 = prev1;
+            prev1=curr;  
+        }
+        return prev1;
     }
 };

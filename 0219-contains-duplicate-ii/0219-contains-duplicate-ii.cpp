dd53d0e38ecbc6b30efbbd204aabbc8cf7d5b2a0
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2024-03-12 02:05:07 +0530

    Time: 143 ms (30.57%), Space: 83.8 MB (9.68%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<=i+k;j++){
-                if(j>=nums.size()){
-                    break;
-                }
-                if(nums[i]==nums[j]){
-                    return true;
-                }
+        set<int> s;
+        if(nums.size()<=k){
+            for(int i=0; i<nums.size(); i++){
+                s.insert(nums[i]);
             }
+            if(s.size()<nums.size()) return true;
+            else return false;
         }
-        return false;
-    }
+        
+        for(int i=0; i<=k; i++){
+            s.insert(nums[i]);
+        }
+        cout<<s.size();
+        if(s.size()<k+1) return true;
+        
+        int x=0;
+        for(int i=k+1; i<nums.size(); i++){
+            s.erase(nums[x]);
+            x++;
+            s.insert(nums[i]);
+            if(s.size()<k+1) return true;
+        }
+        
+        return false;
+    }
+
 };

e0216d977ccc85ee6c0a46a2e9ea184649a6726a
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2024-09-17 02:47:08 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (74.37%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    
+private:
+    double binaryExp(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+       
+        if (n < 0) {
+            return 1.0 / binaryExp(x, -n);
+        }
+       
+        if (n % 2 == 1) {
+            return x * binaryExp(x * x, (n - 1) / 2);
+        } else {
+            return binaryExp(x * x, n / 2);
+        }
+    }
+    
+public:
+    double myPow(double x, int n) {
+       return binaryExp(x, (long)n);
+    }
+};

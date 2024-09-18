405efcfc0ec78c85322f373cee04ed363ceecac0
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2024-09-18 19:19:33 +0530

    Time: 981 ms (16.22%), Space: 34 MB (15.97%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
-    
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         unordered_set<string> dict(wordList.begin(), wordList.end());
-        queue<string> todo;
-        todo.push(beginWord);
-        int ladder = 1;
-        while (!todo.empty()) {
-            int n = todo.size();
-            for (int i = 0; i < n; i++) {
-                string word = todo.front();
-                todo.pop();
+        queue<string> q;
+        q.push(beginWord);
+        int res = 1;
+        
+        while(!q.empty()){
+          
+            int x = q.size();
+            
+            for(int i=0; i<x; i++){
+                string word = q.front();
+                q.pop();
                 if (word == endWord) {
-                    return ladder;
+                    return res;
                 }
                 dict.erase(word);
+
                 for (int j = 0; j < word.size(); j++) {
                     char c = word[j];
                     for (int k = 0; k < 26; k++) {
                         word[j] = 'a' + k;
                         if (dict.find(word) != dict.end()) {
-                            todo.push(word);
+                            q.push(word);
                         }
                      }
                     word[j] = c;
                 }
             }
-            ladder++;
+            res++;
         }
         return 0;
-
     }
 };

0684f7756bcb1351e341ad9101aedd8914599443
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2024-09-21 21:18:06 +0530

    Time: 19 ms (10.24%), Space: 10 MB (83.04%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        int length = s.length();
-        if (length == 0) {
-            return s;
-        }
+        string reversedString = s;
+        reverse(reversedString.begin(), reversedString.end());
 
-        // Find the longest palindromic prefix
-        int left = 0;
-        for (int right = length - 1; right >= 0; right--) {
-            if (s[right] == s[left]) {
-                left++;
+        for (int i = 0; i < s.size(); ++i) {
+            if (!memcmp(s.c_str(), reversedString.c_str() + i, s.size() - i)) {
+                return reversedString.substr(0, i) + s;
             }
         }
-
-        // If the whole string is a palindrome, return the original string
-        if (left == length) {
-            return s;
-        }
-
-        // Extract the suffix that is not part of the palindromic prefix
-        string nonPalindromeSuffix = s.substr(left);
-        string reverseSuffix =
-            string(nonPalindromeSuffix.rbegin(), nonPalindromeSuffix.rend());
-
-        // Form the shortest palindrome by prepending the reversed suffix
-        return reverseSuffix + shortestPalindrome(s.substr(0, left)) +
-               nonPalindromeSuffix;
+        
+        return reversedString + s;
     }
 };

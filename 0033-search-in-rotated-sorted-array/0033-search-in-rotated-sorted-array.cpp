858c81afa88be03bbc234d19f1f72b973c2810c5
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2024-09-24 01:01:45 +0530

    Time: 5 ms (18.63%), Space: 15.6 MB (50.09%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,33 +1,42 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size()-1;
-        while (l <= r) {
-            int mid = (l+r) / 2;
-            if (target == nums[mid])
-                return mid;
-            // there exists rotation; the middle element is in the left part of the array
-            if (nums[mid] > nums[r]) {
-                if (target < nums[mid] && target >= nums[l])
-                    r = mid - 1;
-                else
-                    l = mid + 1;
-            }
-            // there exists rotation; the middle element is in the right part of the array
-            else if (nums[mid] < nums[l]) {
-                if (target > nums[mid] && target <= nums[r])
-                    l = mid + 1;
-                else
-                    r = mid - 1;
-            }
-            // there is no rotation; just like normal binary search
-            else {
-                if (target < nums[mid])
-                    r = mid - 1;
-                else
-                    l = mid + 1;
-            }
+    int bst(int i, int n, vector<int> nums, int target){
+        
+        cout<<i<<" "<<n<<endl;
+        if(i<0 || n>=nums.size()) return -1;
+        
+        if(nums[i]==target) return i;
+        if(nums[n]==target) return n;
+        
+        if(i==n || n-i==1) return -1;
+        
+        int mid = (n+i)/2;
+        if(nums[mid]==target) return mid;
+        
+        int res=-1;
+        
+        if(nums[i]<nums[mid] && nums[mid]>target && nums[i]<target){
+            res = bst(i, mid, nums, target); 
         }
-        return -1;
+        
+        if(res==-1 && nums[mid]<nums[n] && nums[mid]<target && nums[n]>target){
+            res = bst(mid+1, n, nums, target);
+        }
+        
+        if(res==-1 && nums[i]>nums[mid] && (nums[mid]>target || nums[i]<target)){
+            res = bst(i, mid, nums, target); 
+        }
+        
+        if(res==-1 && nums[mid]>nums[n] && (nums[mid]<target || nums[n]>target)){
+            res = bst(mid+1, n, nums, target);
+        }
+        
+        return res;
+    }
+    
+    int search(vector<int>& nums, int target) {
+        
+        return bst(0, nums.size()-1, nums, target);
+           
     }
 };

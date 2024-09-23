129a41a358547e750765393d48d30354c5c5b807
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2024-09-24 01:11:52 +0530

    Time: 0 ms (100.00%), Space: 14 MB (50.38%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,42 +1,32 @@
 class Solution {
 public:
-    int bst(int i, int n, vector<int> nums, int target){
-        
-        cout<<i<<" "<<n<<endl;
-        if(i<0 || n>=nums.size()) return -1;
-        
-        if(nums[i]==target) return i;
-        if(nums[n]==target) return n;
-        
-        if(i==n || n-i==1) return -1;
-        
-        int mid = (n+i)/2;
-        if(nums[mid]==target) return mid;
-        
-        int res=-1;
-        
-        if(nums[i]<nums[mid] && nums[mid]>target && nums[i]<target){
-            res = bst(i, mid, nums, target); 
-        }
-        
-        if(res==-1 && nums[mid]<nums[n] && nums[mid]<target && nums[n]>target){
-            res = bst(mid+1, n, nums, target);
-        }
-        
-        if(res==-1 && nums[i]>nums[mid] && (nums[mid]>target || nums[i]<target)){
-            res = bst(i, mid, nums, target); 
-        }
-        
-        if(res==-1 && nums[mid]>nums[n] && (nums[mid]<target || nums[n]>target)){
-            res = bst(mid+1, n, nums, target);
-        }
-        
-        return res;
-    }
     
     int search(vector<int>& nums, int target) {
         
-        return bst(0, nums.size()-1, nums, target);
-           
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
     }
 };

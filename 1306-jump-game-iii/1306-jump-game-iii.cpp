3222889b4fedb414b650b5ba2092fc5fd94097b4
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2024-09-30 19:22:57 +0530

    Time: 29 ms (79.54%), Space: 40.2 MB (31.46%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,8 +1,24 @@
 class Solution {
 public:
+    void recurr(vector<int>& arr, int cur, vector<int>& vis, int& i){
+        vis[cur]=1;
+        if(arr[cur]!=0){
+            if(cur+arr[cur]<arr.size() && vis[cur+arr[cur]]==0 && i==INT_MAX){
+                recurr(arr, cur+arr[cur], vis, i);
+            }
+            if(cur-arr[cur]>=0 && vis[cur-arr[cur]]==0 && i==INT_MAX){
+                recurr(arr, cur-arr[cur], vis, i);
+            }
+        }
+        else i = cur;
+    }
+    
     bool canReach(vector<int>& arr, int cur) {
-        if(cur < 0 || cur >= size(arr) || arr[cur] < 0) return false;                  // out of bounds OR already visited ? return false
-        arr[cur] *= -1;       // mark as visited by making -ve
-        return !arr[cur] || canReach(arr, cur + arr[cur]) || canReach(arr, cur - arr[cur]);  // return true if A[cur] == 0 or recurse for both possible jumps
+        vector<int> vis(arr.size(), 0);
+        int i = INT_MAX;
+        recurr(arr, cur, vis, i);
+        if(i!=INT_MAX) return true;
+        
+        return false;
     }
 };

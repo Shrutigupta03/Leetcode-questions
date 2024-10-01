ceac1710c8622cbb87de95be54ba67cf6a86b6a6
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2024-10-01 20:51:43 +0530

    Time: 35 ms (36.45%), Space: 19.2 MB (45.72%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -2,56 +2,55 @@ class Solution {
 public:
     int largest(vector<int>& heights)
     {
-        stack<pair<int, int>> stk;
-        int res = 0;
+        stack<int> st;
+        int n = heights.size();
+        int right_small[n], left_small[n];
 
-        for (int i = 0; i < heights.size(); i++)
-        {
-            int start = i;
-
-            while (!stk.empty() && stk.top().second > heights[i])
-            {
-                int idx = stk.top().first;
-                int width = i - idx;
-                int height = stk.top().second;
-                stk.pop();
-
-                res = max(res, height * width);
-                start = idx;
+        for(int i=0; i<n; i++){
+            while(!st.empty() && heights[st.top()]>=heights[i]){
+                st.pop();
             }
-            
-            stk.push({start, heights[i]});
+
+            if(st.empty()) left_small[i]=0;
+            else left_small[i]=st.top()+1;
+
+            st.push(i);
+        }
+        while(!st.empty())
+          st.pop();
+
+        for(int i=n-1; i>=0; i--){
+            while(!st.empty() && heights[st.top()]>=heights[i]){
+                st.pop();
+            }
+
+            if(st.empty()) right_small[i]=n-1;
+            else right_small[i]=st.top()-1;
+
+            st.push(i);
         }
 
-        while (!stk.empty())
-        {
-            int width = heights.size() - stk.top().first;
-            int height = stk.top().second;
-            stk.pop();
-
-            res = max(res, height * width);
+        int maxA=0;
+        for(int i=0; i<n; i++){
+            maxA = max(maxA, heights[i] * (right_small[i] - left_small[i] + 1));
         }
-        return res;
+        return maxA;
     }
     
     int maximalRectangle(vector<vector<char>>& matrix) {
 
         vector<vector<int>> mat(matrix.size(), vector<int>(matrix[0].size(), 0));
-        for (int j = 0; j < matrix[0].size(); j++)
-        {
-            for (int i = 0; i < matrix.size(); i++)
-            {
-                if (matrix[i][j] == '1')
-                {
+        
+        for (int j = 0; j < matrix[0].size(); j++){
+            for (int i = 0; i < matrix.size(); i++){
+                if (matrix[i][j] == '1'){
                     mat[i][j] = (i == 0) ? 1 : mat[i - 1][j] + 1;
                 }
             }
         }
 
-
         int res = INT_MIN;
-        for (auto mat1: mat)
-        {
+        for (auto mat1: mat){
             res = max(res, largest(mat1));
         }
 

063f4619cc9e58cb87fbe3019bbc18d770fdafe7
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2024-10-01 20:51:11 +0530

    Time: 127 ms (27.70%), Space: 82.3 MB (60.20%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        stack<int> st;
-   int n = heights.size();
-   int right_small[n], left_small[n];
-  for(int i=0; i<n; i++){
-   while(!st.empty() && heights[st.top()]>=heights[i]){
-     st.pop();
-   }
-   if(st.empty())
-     left_small[i]=0;
-   else
-     left_small[i]=st.top()+1;
-    st.push(i);
-  }
-    while(!st.empty())
-      st.pop();
+        stack<pair<int, int>> stk;
+        int res = 0;
 
-  for(int i=n-1; i>=0; i--){
-    while(!st.empty() && heights[st.top()]>=heights[i]){
-     st.pop();
-    }
-    if(st.empty())
-     right_small[i]=n-1;
-    else
-     right_small[i]=st.top()-1;
-    st.push(i);
-  }
+        for (int i = 0; i < heights.size(); i++)
+        {
+            int start = i;
 
-  int maxA=0;
-  for(int i=0; i<n; i++){
-    maxA = max(maxA, heights[i] * (right_small[i] - left_small[i] + 1));
-  }
+            while (!stk.empty() && stk.top().second > heights[i])
+            {
+                int idx = stk.top().first;
+                int width = i - idx;
+                int height = stk.top().second;
+                stk.pop();
 
-  return maxA;
+                res = max(res, height * width);
+                start = idx;
+            }
+            
+            stk.push({start, heights[i]});
+        }
+
+        while (!stk.empty())
+        {
+            int width = heights.size() - stk.top().first;
+            int height = stk.top().second;
+            stk.pop();
+
+            res = max(res, height * width);
+        }
+        return res;
     }
 };

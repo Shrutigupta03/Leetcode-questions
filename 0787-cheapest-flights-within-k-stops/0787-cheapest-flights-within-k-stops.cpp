96df98cec4a9ceec9c1c16591a1e3ca4f04d46e9
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2024-10-06 16:18:30 +0530

    Time: 9 ms (95.35%), Space: 17.5 MB (27.47%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -2,39 +2,39 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) 
     {
-        vector<vector<pair<int,int>>>graph(n,vector<pair<int,int>>());
-
-        for(auto it:flights)
-        {
-            graph[it[0]].push_back({it[1],it[2]});
-        }       
-        
-        vector<int>min_stops(n,INT_MAX);
-
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
-        pq.push({0,src,0});
-
-        while(!pq.empty())
-        {
-            auto it=pq.top();
-            pq.pop();
-
-            int dist=it[0];
-            int node=it[1];
-            int stops=it[2];
-
-            if(stops>min_stops[node] || stops>k+1) continue;
-            
-            min_stops[node]=stops;
-            if(node==dst) return dist;
-
-            for(auto it:graph[node])
-            {
-                int neigh=it.first;
-                int distance=it.second;
-                pq.push({dist+distance,neigh,stops+1});        
-            }
+        vector<vector<pair<int, int>>> adj(n);
+        for(auto it : flights){
+            adj[it[0]].push_back({it[1], it[2]});
         }
-        return -1;
+        
+        vector<int> dist(n, -1);
+        queue<pair<int, int>> pq;
+        pq.push({src, 0});
+        ++k;
+        
+        while(!pq.empty()){
+            if(!k) break;
+            int len = pq.size();
+            
+            for(int i = 0; i < len; i++) {
+                
+                auto cur = pq.front(); 
+                pq.pop();
+                
+                for(auto it: adj[cur.first]) {
+                    
+                    int price = cur.second + it.second; 
+                    
+                    if(dist[it.first] == -1 || price < dist[it.first]) {
+                        dist[it.first] = price;
+                        pq.push({it.first, price});
+                    }
+                }
+                
+            }
+            k--;
+        }
+        
+        return dist[dst];
     }
 };
